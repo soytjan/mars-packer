d90486182bb66e5ebe752bfa0322480093c3e8c6
Amanda Tjan <amanda.tjan@gmail.com>
2018-04-02 10:34:30 -0600

    Write GET items endpoint

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-// const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('./knexfile')[environment];
-// const database = require('knex')(configuration);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Mars Packer DB';
@@ -26,6 +26,17 @@ app.use(express.static('public'))
 
 app.get('/', (request, response) => {
 
+});
+
+// items endpoints
+app.get('/api/v1/items', (request, response) => {
+  database('items').select()
+    .then((items) => {
+      response.status(200).json(items);
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    });
 });
 
 app.listen(app.get('port'), () => {
