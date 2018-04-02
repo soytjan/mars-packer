02a72b6d0b9aae781d4963c4673f69af9fb71ad4
Amanda Tjan <amanda.tjan@gmail.com>
2018-04-02 10:28:03 -0600

    Setup and run intial migration, set up and run development seed file

---
+++ db/migrations/20180402100015_initial.js
@@ -0,0 +1,19 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('items', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.boolean('packed');
+
+      table.timestamps(true, true);
+    })
+
+  ])
+};
+
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('items')
+  ]);
+};
